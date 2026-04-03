f88ef1a8942b10b3d3fb1d791b0539e4f9a587dd
Lucas-le-pro <guylucas29042015@gmail.com>
2026-04-03 16:53:06 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
@@ -8,6 +9,7 @@ const io = new Server(server, {
   cors: { origin: '*' }
 });
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('LamaaGames server OK'));
